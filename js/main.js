fa8784d3ef9377032f1677a3625f5057efb41bc8
Harry Thuku <harsygeorgee@gmail.com>
2018-07-16 09:27:24 +0300

    first batch of business logic added in js. concept still not tested

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-// $(document).ready(function(){
-//     alert("hey there. i work")
-// })
+//Business Logic ::
+function results(intValue){
+    for(var i=1; i<=intValue; i++){
+        if(((i % 3) === 0) && ((i % 5) === 0)){
+            $('#result').append("<li class='pingpong'>" + "Entry: " + i + ", status: " + "pingpong</li>");
+            pingPongCount+=1;
+            $('#pingpongCount').text(pingPongCount);
+        }
+        else if((i % 5) === 0){
+            $('#result').append("<li class='pong'>" + "Entry: " + i + ", status: " + "pong</li>");
+            pongCount+=1;
+            $('#pongCount').text(pongCount);
+        }
+        else if((i % 3)===0){
+            $('#result').append("<li class='ping'>" + "Entry: " + i + ", status: " + "ping</li>");
+            pingCount+=1;
+            $('#pingCount').text(pingCount);
+        }
+        else{
+            $('#result').append("<li>" + "Entry: " + i + ", status: " + "neither</li>");
+            neitherCount+=1;
+            $('#neither').text(neitherCount);
+        }
+    }
+    i=i+1;
+}
